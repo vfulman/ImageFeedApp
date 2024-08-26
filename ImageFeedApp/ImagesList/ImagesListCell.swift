3666c9d37d32b57d49b69a4e18f388459a713584
Vitaliy Fulman <vitaliyfulman@gmail.com>
2024-08-26 16:45:11 +0700

    Рефакторинг. Исправил замечания из PR. Добавил градиент под дату на картинке.

--- ImageFeedApp/ImagesList/ImagesListCell.swift
+++ ImageFeedApp/ImagesList/ImagesListCell.swift
@@ -12,6 +12,25 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet var likeButton: UIButton!
     @IBOutlet var dateStamp: UILabel!
     
+    private let gradientView: UIView = UIView()
+    
     static let reuseIdentifier = "ImagesListCell"
-
+    
+    func addGradientIfNeeded() {
+        guard gradientView.superview == nil else { return }
+        let gradientHeight = 30.0
+        
+        contentImage.addSubview(gradientView)
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        gradientView.leadingAnchor.constraint(equalTo: contentImage.leadingAnchor).isActive = true
+        gradientView.trailingAnchor.constraint(equalTo: contentImage.trailingAnchor).isActive = true
+        gradientView.bottomAnchor.constraint(equalTo: contentImage.bottomAnchor).isActive = true
+        gradientView.heightAnchor.constraint(equalToConstant: gradientHeight).isActive = true
+        contentImage.clipsToBounds = true
+        
+        let gradient = CAGradientLayer()
+        gradient.frame = CGRect(x: 0, y: 0, width: contentView.bounds.width, height: gradientHeight)
+        gradient.colors = [UIColor.clear.cgColor, UIColor.ypBlack.withAlphaComponent(0.2).cgColor]
+        gradientView.layer.addSublayer(gradient)
+    }
 }
--- ImageFeedApp/ImagesList/ImagesListViewController.swift
+++ ImageFeedApp/ImagesList/ImagesListViewController.swift
@@ -7,36 +7,36 @@
 
 import UIKit
 
-class ImagesListViewController: UIViewController {
+final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    private let currentDate = Date()
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
         formatter.timeStyle = .none
         formatter.locale = Locale(identifier: "ru_RU")
-
+        
         return formatter
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
     
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+    private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
             return
         }
         cell.contentImage.image = image
-        cell.dateStamp.text = dateFormatter.string(from: Date())
+        cell.dateStamp.text = dateFormatter.string(from: currentDate)
         let likeImage = indexPath.row % 2 == 0 ? UIImage(named: "like_on") : UIImage(named: "like_off")
         cell.likeButton.setImage(likeImage, for: .normal)
+        cell.addGradientIfNeeded()
     }
-    
 }
 
 extension ImagesListViewController: UITableViewDataSource {
@@ -52,12 +52,15 @@ extension ImagesListViewController: UITableViewDataSource {
         }
         
         configCell(for: imageListCell, with: indexPath)
+        
         return imageListCell
     }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: - Добавить логику при нажатии на ячейку
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
@@ -65,8 +68,7 @@ extension ImagesListViewController: UITableViewDelegate {
         }
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-        let imageWidth = image.size.width
-        let scale = imageViewWidth / imageWidth
+        let scale = imageViewWidth / image.size.width
         let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
         return cellHeight
     }
