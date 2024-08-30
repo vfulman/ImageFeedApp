3c73200f385d6245400e5e79a3665955d346b1df
Vitaliy Fulman <vitaliyfulman@gmail.com>
2024-08-30 15:06:42 +0700

    Добавлена проверка размерностей картинки, чтобы избежать ошибки деления на ноль

--- ImageFeedApp/SingleImage/SingleImageViewController.swift
+++ ImageFeedApp/SingleImage/SingleImageViewController.swift
@@ -39,6 +39,9 @@ final class SingleImageViewController: UIViewController {
         view.layoutIfNeeded()
         let visibleRectSize = scrollView.bounds.size
         let imageSize = image.size
+        
+        guard imageSize.width != 0, imageSize.height != 0 else { return }
+        
         let hScale = visibleRectSize.width / imageSize.width
         let vScale = visibleRectSize.height / imageSize.height
         let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
